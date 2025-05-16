36afb68026860df4e6a4afd56208be1a5441e97f
PedroHQO <pedro39henrique.q.o@gmail.com>
2025-05-16 00:05:07 -0300

    Atualizando portfolio, adicionando menus e form

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ window.addEventListener('scroll', () =>{
         }
     });
 })
+
+document.getElementById('formContato').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const contato = document.getElementById('contato').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    const textoWhatsApp = `Olá Pedro , vim pelo seu portfólio!%0A%0A*Nome:* ${nome}%0A*E-mail:* ${email}%0A*Mensagem:* ${mensagem}`;
+
+    window.open(`https://wa.me/5535999256938?text=${textoWhatsApp}`, '_blank');
+
+    this.reset();
+
+    alert('Obrigado pela mensagem! Você será redirecionado para o WhatsApp.')
+})
